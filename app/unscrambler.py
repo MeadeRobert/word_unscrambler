e50ca6b8800260919bfd7f4415a5dde3d184d864
MeadeRobert <meade.develop3r@gmail.com>
2017-11-22 23:36:19 -0500

    it works (sort of)

---
+++ app/unscrambler.py
@@ -0,0 +1,32 @@
+import sys
+import enchant
+from itertools import permutations
+
+# get valie words from list in desired language
+def get_valid_words(words, lang):
+  valid_words = []
+  dict = enchant.Dict(lang)
+  for i in words:
+    if dict.check(i):
+      valid_words.append(i)
+  print "Completed:", lang
+  return valid_words
+
+
+def unscramble(letters, languages):
+  print languages
+  # generate possible words with more then 2 chars O(n!) :(
+  perm = [''.join(p) for p in permutations(letters)]
+  words = set(perm)
+  # get all permutations of lesser length than number of letters
+  for i in range(2, len(letters)):
+    words |= set([p[0:i] for p in perm])
+
+  # check against desired languages
+  results = {}
+  for lang in languages:
+    results[lang] = get_valid_words(words, lang)
+  
+  return results
+
+#print unscramble('dogs', ['en_US'])
--- app/views.py
+++ app/views.py
@@ -1,13 +1,20 @@
 from flask import render_template, flash, redirect
 from app import app
-from .forms import LoginForm
+from .forms import LetterForm
+from . import unscrambler
+import re
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  form = LoginForm()
-  return render_template('login.html',title='Sign In',form=form)
+  form = LetterForm()
+  results = ''
+  if form.validate_on_submit():
+    print "submitted"
+    print form.letters.data
+    print form.languages.data
+    result_dict = unscrambler.unscramble(form.letters.data, re.findall("[^\s]+", form.languages.data))
+    print result_dict
+    results = ''
+    for i in result_dict.keys():
+      results += i + ": " + ', '.join(result_dict[i]) + '<br>'
+  return render_template('index.html', form=form, results=results)
