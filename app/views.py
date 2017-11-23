367a25a6d8d0578066272092b7c7d16f331131eb
MeadeRobert <meade.develop3r@gmail.com>
2017-11-23 02:16:07 -0500

    added dictionary list to webpage and organized results better by length and language

--- app/views.py
+++ app/views.py
@@ -2,19 +2,30 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LetterForm
 from . import unscrambler
+import enchant
 import re
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
   form = LetterForm()
   results = ''
+  # unscramble
   if form.validate_on_submit():
     print "submitted"
     print form.letters.data
     print form.languages.data
+    
+    # get results
     result_dict = unscrambler.unscramble(form.letters.data, re.findall("[^\s]+", form.languages.data))
     print result_dict
+
+    # make human readable and embed in page
     results = ''
-    for i in result_dict.keys():
-      results += i + ": " + ', '.join(result_dict[i]) + '<br>'
-  return render_template('index.html', form=form, results=results)
+    for lang in result_dict.keys():
+      freq_dict = {i:[] for i in range(2, len(form.letters.data)+1)}
+      for word in result_dict[lang]:
+        freq_dict[len(word)].append(word)
+      results += "<h3>"+ lang + ":</h3>" + '<br>'.join([', '.join(sorted(freq_dict[i])) for i in range(2, len(form.letters.data)+1)]) 
+  
+  # write page and pass relevant values
+  return render_template('index.html', form=form, results=results, languages=enchant.list_languages())
